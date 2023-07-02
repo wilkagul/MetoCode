1d50421c98644725fab3b960178f439b44266d61
wilkagul <wilkagul@gmail.com>
2023-07-02 12:49:49 +0300

    unity yeni input sistemiyle touch hareketi

---
+++ Assets/Script/Input/InputManager.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[DefaultExecutionOrder(-1)]
+public class InputManager : Singleton<InputManager>
+{
+    public delegate void StartTouch(Vector2 position, float time);
+    public event StartTouch OnStartTouch;
+    public delegate void EndTouch(Vector2 position, float time);
+    public event EndTouch OnEndTouch;
+
+    private PlayerControls playerControl;
+    private Camera mainCamera;
+
+    private void Awake()
+    {
+        playerControl = new PlayerControls();
+        mainCamera = Camera.main;
+    }
+
+    private void OnEnable()
+    {
+        playerControl.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControl.Disable();
+    }
+    private void Start()
+    {
+        playerControl.Touch.PrimaryContact.started += ctx => StartTouchPrimary(ctx);
+        playerControl.Touch.PrimaryContact.canceled += ctx => EndTouchPrimary(ctx);
+    }
+
+    private void StartTouchPrimary(InputAction.CallbackContext context)
+    {
+        if (OnStartTouch != null) OnStartTouch(Utils.ScreenToWorld(mainCamera, playerControl.Touch.PrimaryPosition.ReadValue<Vector2>()), (float)context.startTime);
+    }
+
+    private void EndTouchPrimary(InputAction.CallbackContext context)
+    {
+        if (OnEndTouch != null) OnEndTouch(Utils.ScreenToWorld(mainCamera, playerControl.Touch.PrimaryPosition.ReadValue<Vector2>()), (float)context.time);
+    }
+
+    public Vector2 PrimaryPosition()
+    {
+        return Utils.ScreenToWorld(mainCamera, playerControl.Touch.PrimaryPosition.ReadValue<Vector2>());
+    }
+}
---
+++ Assets/Script/Input/SwipeDetection.cs
@@ -0,0 +1,83 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeDetection : MonoBehaviour
+{
+    [SerializeField] private float mininumDistance = .2f;
+    [SerializeField] private float maximumTime = 1f;
+    [SerializeField, Range(0f, 1f)] private float directionThreshold = .9f;
+
+    private InputManager inputManager;
+
+    private Vector2 startPosition;
+    private float startTime;
+    private Vector2 endPosition;
+    private float endTime;
+
+    private void Awake()
+    {
+        inputManager = InputManager.Instance;
+    }
+
+    private void OnEnable()
+    {
+        inputManager.OnStartTouch += SwipeStart;
+        inputManager.OnEndTouch += SwipeEnd;
+    }
+
+    private void OnDisable()
+    {
+        inputManager.OnStartTouch -= SwipeStart;
+        inputManager.OnEndTouch -= SwipeEnd;
+    }
+
+    private void SwipeStart(Vector2 position, float time)
+    {
+        startPosition = position;
+        startTime = time;
+    }
+
+    private void SwipeEnd(Vector2 position, float time)
+    {
+        endPosition = position;
+        endTime = time;
+        DetectSwipe();
+    }
+
+    private void DetectSwipe()
+    {
+        if (Vector3.Distance(startPosition, endPosition) >= mininumDistance && (endTime - startTime) <= maximumTime)
+        {
+            Debug.Log("Swipe Detected");
+            Debug.DrawLine(startPosition, endPosition, Color.red, 5f);
+
+            Vector3 direction = endPosition - startPosition;
+            Vector2 direction2D = new Vector3(direction.x, direction.y).normalized;
+            SwipeDirection(direction2D);
+        }
+    }
+
+    private void SwipeDirection(Vector2 direction)
+    {
+        if (Vector2.Dot(Vector2.up, direction) > directionThreshold)
+        {
+            // Debug.Log("Swipe Up");
+        }
+        else if (Vector2.Dot(Vector2.down, direction) > directionThreshold)
+        {
+            // Debug.Log("Swipe Down");
+        }
+        else if (Vector2.Dot(Vector2.left, direction) > directionThreshold)
+        {
+            // Debug.Log("Swipe Left");
+        }
+        else if (Vector2.Dot(Vector2.right, direction) > directionThreshold)
+        {
+            // Debug.Log("Swipe Right");
+        }
+        else
+        {
+            // Debug.Log("Swipe Error");
+        }
+    }
+}
